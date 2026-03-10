3c43cccf4f04c7688b84970479d3cf9e42bbcb09
I-k-a-r-o-s <141158707+I-k-a-r-o-s@users.noreply.github.com>
2026-03-10 11:30:34 +0530

    added more routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import { Route, Routes } from "react-router";
 import Home from "./pages/Home";
+import About from "./pages/About";
+import Cart from "./pages/Cart";
+import Contact from "./pages/Contact";
+import Menu from "./pages/Menu";
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home/>}/>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/cart" element={<Cart />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="/menu" element={<Menu />} />
     </Routes>
   );
 };
