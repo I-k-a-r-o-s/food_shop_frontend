2cf96cfd617a313939359fba446e794787382cbd
I-k-a-r-o-s <141158707+I-k-a-r-o-s@users.noreply.github.com>
2026-03-10 12:13:01 +0530

    restructured and added navbar and react hot toast

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,22 @@ import About from "./pages/About";
 import Cart from "./pages/Cart";
 import Contact from "./pages/Contact";
 import Menu from "./pages/Menu";
+import Navbar from "./components/Navbar";
+import { Toaster } from "react-hot-toast";
 
 const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/cart" element={<Cart />} />
-      <Route path="/contact" element={<Contact />} />
-      <Route path="/menu" element={<Menu />} />
-    </Routes>
+    <div>
+      <Toaster position="bottom-center" reverseOrder={false} />
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/menu" element={<Menu />} />
+      </Routes>
+    </div>
   );
 };
 export default App;
