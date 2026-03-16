a21d23f85b0edf8e40ec8284886b26f7c4fbb514
I-k-a-r-o-s <141158707+I-k-a-r-o-s@users.noreply.github.com>
2026-03-16 18:13:05 +0530

    base structure of loginmodal with placeholder functios

--- src/components/LoginModal.jsx
+++ src/components/LoginModal.jsx
@@ -1,6 +1,28 @@
+import { useState } from "react";
+import toast from "react-hot-toast";
+import { FaRegEye, FaRegEyeSlash } from "react-icons/fa";
+import { LuLogIn } from "react-icons/lu";
+import { TbUserPlus } from "react-icons/tb";
+import { Link } from "react-router";
+
 const LoginModal = () => {
+  const [loading, setLoading] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
   const handleSubmit = (e) => {
     e.preventDefault();
+    try {
+      setLoading(true);
+      console.log(formData);
+      toast.success("Login Successfull!");
+    } catch (error) {
+      console.log("Error in handleSubmit:", error);
+    } finally {
+      setLoading(false);
+    }
   };
   return (
     <div>
@@ -19,7 +41,7 @@ const LoginModal = () => {
           </h3>
 
           {/* Center form and allow it to take full width */}
-          <div className="mt-6 flex justify-center">
+          <div className="mt-6 flex items-center flex-col">
             <form onSubmit={handleSubmit} className="w-full">
               <fieldset className="w-full space-y-4">
                 <div className="space-y-1">
@@ -30,6 +52,13 @@ const LoginModal = () => {
                     type="email"
                     className="input input-bordered w-full validator"
                     placeholder="Email"
+                    value={formData.email}
+                    onChange={(e) =>
+                      setFormData((prev) => ({
+                        ...prev,
+                        email: e.target.value,
+                      }))
+                    }
                   />
                 </div>
 
@@ -37,14 +66,40 @@ const LoginModal = () => {
                   <label className="label">
                     <span className="label-text">Password</span>
                   </label>
-                  <input
-                    type="password"
-                    className="input input-bordered w-full validator"
-                    placeholder="Password"
-                  />
+                  <div className="relative">
+                    <input
+                      type={showPassword ? "text" : "password"}
+                      className="input input-bordered w-full validator pr-10"
+                      placeholder="Password"
+                      value={formData.password}
+                      onChange={(e) =>
+                        setFormData((prev) => ({
+                          ...prev,
+                          password: e.target.value,
+                        }))
+                      }
+                    />
+                    <button
+                      type="button"
+                      title={showPassword ? "Hide password" : "Show password"}
+                      onClick={() => setShowPassword(!showPassword)}
+                      className="absolute right-2 top-1/2 -translate-y-1/2 pr-2 hover:cursor-pointer"
+                    >
+                      {showPassword ? <FaRegEye /> : <FaRegEyeSlash />}
+                    </button>
+                  </div>
                 </div>
 
-                <button className="btn btn-neutral w-full mt-2" type="submit">Login</button>
+                <button className="btn btn-primary w-full mt-2" type="submit">
+                  {loading ? (
+                    <span className="loading loading-ring loading-lg"></span>
+                  ) : (
+                    <>
+                      Login
+                      <LuLogIn size={20} />
+                    </>
+                  )}
+                </button>
               </fieldset>
             </form>
           </div>
@@ -55,6 +110,15 @@ const LoginModal = () => {
               <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
                 ✕
               </button>
+              <Link
+                to={"/register"}
+                onClick={() => document.getElementById("login_modal").close()}
+              >
+                <span className="btn btn-ghost">
+                  <TbUserPlus />
+                  Create New Account
+                </span>
+              </Link>
             </form>
           </div>
         </div>
