2f69246b4c575d13325abc6e767d36aba356a656
I-k-a-r-o-s <141158707+I-k-a-r-o-s@users.noreply.github.com>
2026-03-17 16:23:37 +0530

    seperated the welcome screen and imported to homepage

---
+++ src/components/Banner.jsx
@@ -0,0 +1,48 @@
+import { useState } from "react";
+import { FaArrowDown } from "react-icons/fa";
+import { IoSearchOutline } from "react-icons/io5";
+
+const Banner = () => {
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    console.log("Search?", searchQuery);
+  };
+  return (
+    <div className="hero min-h-[calc(100vh-4rem)] bg-base-100">
+      <div className="hero-content text-center">
+        <div className="max-w-md">
+          <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+          <p className="mb-5">
+            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
+            a id nisi.
+          </p>
+          <form onSubmit={handleSearch}>
+            <div className="join">
+              <label className="input join-item">
+                <IoSearchOutline size={20} />
+                <input
+                  type="search"
+                  required
+                  placeholder="Discover"
+                  value={searchQuery}
+                  onChange={(e) => setSearchQuery(e.target.value)}
+                />
+              </label>
+              <button type="submit" className="btn btn-primary join-item">
+                Search
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+      <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 flex flex-col items-center">
+        <span className="mb-2">Scoll</span>
+        <FaArrowDown className="h-5 w-5 animate-bounce" />
+      </div>
+    </div>
+  );
+};
+export default Banner;
