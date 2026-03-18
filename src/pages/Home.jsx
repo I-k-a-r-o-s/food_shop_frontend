1b1847686112b6184f31e63a05d969f2a6df9db8
I-k-a-r-o-s <141158707+I-k-a-r-o-s@users.noreply.github.com>
2026-03-18 15:51:33 +0530

    added the special offer component. usage unsure

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,11 @@
 import Banner from "../components/Banner";
+import SpecialOffer from "../components/SpecialOffer";
 
 const Home = () => {
   return (
     <>
       <Banner />
-      
+      <SpecialOffer />
     </>
   );
 };
