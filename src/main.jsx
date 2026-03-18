204214092c9aafdcea3ad6882d4b6ee250ca82eb
I-k-a-r-o-s <141158707+I-k-a-r-o-s@users.noreply.github.com>
2026-03-18 17:56:11 +0530

    renamed context

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,14 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router";
-import CartContextProvider from "./context/CartContext.jsx";
+import AppContextProvider from "./context/AppContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
-      <CartContextProvider>
+      <AppContextProvider>
         <App />
-      </CartContextProvider>
+      </AppContextProvider>
     </BrowserRouter>
   </StrictMode>,
 );
