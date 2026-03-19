d568a11fd46d8bba4dde702f80c1e1d495d5d7c4
I-k-a-r-o-s <141158707+I-k-a-r-o-s@users.noreply.github.com>
2026-03-19 18:14:33 +0530

    food card

--- src/components/FoodCard.jsx
+++ src/components/FoodCard.jsx
@@ -1,25 +1,72 @@
-const FoodCard = () => {
+import { useContext } from "react";
+import { appContext } from "../context/AppContext";
+import { LiaMinusCircleSolid, LiaPlusCircleSolid } from "react-icons/lia";
+
+const FoodCard = ({ food }) => {
+  const { currency, cartItems, addToCart, removeFromCart } =
+    useContext(appContext);
+
   return (
     <div className="card bg-base-100 w-96 shadow-sm">
-      <figure>
-        <img src="" alt="" />
-      </figure>
+      <div className="hover-3d">
+        <figure className="px-5 pt-5">
+          <img src={food.image} alt={food.name} />
+        </figure>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
       <div className="card-body">
         <div className="flex justify-between">
-          <h2 className="card-title text-2xl">
-            Title
-            <div className="badge badge-secondary">NEW</div>
-          </h2>
-
-          <span className="text-xl">$29/mo</span>
+          <h2 className="card-title text-2xl">{food.name}</h2>
+          <span className="text-xl">
+            {food.price} {currency}
+          </span>
         </div>
-        <p>
-          A card component has a figure, a body part, and inside body there are
-          title and actions parts
-        </p>
-        <div className="card-actions justify-end">
-          <div className="badge badge-outline">Fashion</div>
-          <div className="badge badge-outline">Products</div>
+        <p>{food.description}</p>
+        <div className="card-actions justify-between">
+          <div className="rating rating-sm">
+            <div className="mask mask-star bg-success" aria-current=""></div>
+            <div className="mask mask-star bg-success" aria-current=""></div>
+            <div
+              className="mask mask-star bg-success"
+              aria-current="true"
+            ></div>
+            <div className="mask mask-star bg-success" aria-current=""></div>
+            <div className="mask mask-star bg-success" aria-current=""></div>
+          </div>
+          <div className="flex gap-2">
+            {!cartItems[food._id] ? (
+              <button
+                className="btn   btn-sm"
+                onClick={() => addToCart(food._id)}
+              >
+                <LiaPlusCircleSolid size={20} />
+                Add
+              </button>
+            ) : (
+              <>
+                <button
+                  className="btn btn-circle  btn-sm"
+                  onClick={() => removeFromCart(food._id)}
+                >
+                  <LiaMinusCircleSolid size={20} />
+                </button>
+                <label className="label font-bold">{cartItems[food._id]}</label>
+                <button
+                  className="btn btn-circle  btn-sm"
+                  onClick={() => addToCart(food._id)}
+                >
+                  <LiaPlusCircleSolid size={20} />
+                </button>
+              </>
+            )}
+          </div>
         </div>
       </div>
     </div>
