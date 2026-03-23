60cc3002b6f4ef6c9aabf2d5e784fe290ed15443
I-k-a-r-o-s <141158707+I-k-a-r-o-s@users.noreply.github.com>
2026-03-23 15:28:02 +0530

    redesign navbar to accomodate the removal of unwanted pages

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,42 +2,17 @@ import { AiOutlineHome } from "react-icons/ai";
 import { BiFoodMenu } from "react-icons/bi";
 import { GiChefToque, GiShoppingCart } from "react-icons/gi";
 import { GrMenu } from "react-icons/gr";
-import { LiaQuestionSolid } from "react-icons/lia";
 import { LuPhoneCall } from "react-icons/lu";
 import { Link } from "react-router";
 import ThemeSelector from "./ThemeSelector";
-import { useContext, useState } from "react";
+import { useContext } from "react";
 import { appContext } from "../context/AppContext";
 import LoginModal from "./LoginModal";
 import { IoSearch } from "react-icons/io5";
 
 const Navbar = () => {
   const { itemCount } = useContext(appContext);
-  const [currentPage, setCurrentPage] = useState("");
 
-  const navLinks = [
-    { name: "Home", to: "/", icon: <AiOutlineHome size={20} /> },
-    { name: "Menu", to: "/menu", icon: <BiFoodMenu size={20} /> },
-    { name: "About", to: "/about", icon: <LiaQuestionSolid size={20} /> },
-    { name: "Contact", to: "/contact", icon: <LuPhoneCall size={20} /> },
-  ];
-  const NavItems = ({ links }) =>
-    links.map((link) => (
-      <li
-        key={link.name}
-        className={
-          currentPage === link.name
-            ? "underline underline-offset-5 decoration-wavy"
-            : ""
-        }
-        onClick={() => setCurrentPage(link.name)}
-      >
-        <Link to={link.to}>
-          {link.icon} {link.name}
-        </Link>
-      </li>
-    ));
-  console.log(itemCount)
   return (
     <div className="navbar fixed top-0 left-0 right-0 z-50 bg-base-100 bg-opacity-95 shadow-sm backdrop-blur-sm">
       <div className="navbar-start">
@@ -49,7 +24,21 @@ const Navbar = () => {
             tabIndex={-1}
             className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow gap-3"
           >
-            <NavItems links={navLinks} />
+            <li>
+              <Link to={"/"}>
+                <AiOutlineHome size={20} /> Home
+              </Link>
+            </li>
+            <li>
+              <a href="#menu">
+                <BiFoodMenu size={20} /> Menu
+              </a>
+            </li>
+            <li>
+              <a href="#footer">
+                <LuPhoneCall size={20} /> Contact Us
+              </a>
+            </li>
           </ul>
         </div>
         <Link to={"/"} className="group">
@@ -64,7 +53,21 @@ const Navbar = () => {
       </div>
       <div className="navbar-center hidden lg:flex">
         <ul className="menu menu-horizontal px-1">
-          <NavItems links={navLinks} />
+          <li>
+            <Link to={"/"}>
+              <AiOutlineHome size={20} /> Home
+            </Link>
+          </li>
+          <li>
+            <a href="#menu">
+              <BiFoodMenu size={20} /> Menu
+            </a>
+          </li>
+          <li>
+            <a href="#footer">
+              <LuPhoneCall size={20} /> Contact Us
+            </a>
+          </li>
         </ul>
       </div>
       <div className="navbar-end">
@@ -79,8 +82,12 @@ const Navbar = () => {
           </div>
         </div>
         <div className="dropdown dropdown-center">
-          <div tabIndex={0} role="button" className="btn btn-circle btn-ghost m-1">
-            <IoSearch size={20}/>
+          <div
+            tabIndex={0}
+            role="button"
+            className="btn btn-circle btn-ghost m-1"
+          >
+            <IoSearch size={20} />
           </div>
           <div
             tabIndex={0}
@@ -88,7 +95,7 @@ const Navbar = () => {
           >
             <div className="card-body">
               <label className="input">
-                <IoSearch size={20}/>
+                <IoSearch size={20} />
                 <input type="search" required placeholder="Search" />
               </label>
             </div>
